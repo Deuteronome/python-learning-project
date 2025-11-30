fd7d091d4d24cecec1b7ec43bb35c11187e02cba
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-11-30 17:19:43 +0100

    Objets assesseurs

--- object.py
+++ object.py
@@ -1,10 +1,33 @@
 class Player:
-
+    #constructor
     def __init__(self, pseudo, health, attack):
         self.pseudo = pseudo
         self.health = health
         self.attack = attack
 
+    #assesseurs
+    def get_pseudo(self):
+        return self.pseudo
+
+    def set_pseudo(self, pseudo):
+        self.pseudo = pseudo
+        return self
+
+    def get_health(self):
+        return self.health
+
+    def set_health(self, health):
+        self.health = health
+        return self
+
+    def get_attack(self):
+        return self.attack
+
+    def set_attack(self, attack):
+        self.attack = attack
+        return self
+
+    #specific methods
     def display_player_info(self):
         print(f"Nom du joueur : {self.pseudo}")
         print(f"Points de vie  : {self.health}")
