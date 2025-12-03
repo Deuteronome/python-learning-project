cd73355465c9243ba91f2a314505317d1c79ece2
Deuteronome <o.burcker@e2c-grandlille.fr>
2025-12-03 13:34:45 +0100

    mise en forme DC

--- dungeon_crawler.py
+++ dungeon_crawler.py
@@ -10,27 +10,33 @@ root.iconbitmap("./src/crawler.ico")
 root.configure(bg="#413642")
 
 #frame principale
-frame = tk.Frame(root, bg="white", bd=1, relief=tk.SUNKEN)
+frame = tk.Frame(root, bg="#413642")
 
 #titre
-header=tk.Frame(frame, bg="#c5d3d5")
+header=tk.Frame(root, bg="#c5d3d5")
 title = tk.Label(header, text="Dungeon Crawler", font=("Courrier", 25), fg="#413642", bg="#c5d3d5")
 title.pack(expand=tk.YES, fill=tk.BOTH)
+
+header.pack(fill=tk.X, ipady=20, side=tk.TOP)
 
 #canva
 width = 300
 height = 300
 image = tk.PhotoImage(file="./src/donjon.png").zoom(18).subsample(32)
-canvas = tk.Canvas(frame, width=width, height=height, background="#413642", bd=0, highlightthickness=0)
+canvas = tk.Canvas(frame, width=width, height=height, background="white", bd=0, highlightthickness=0)
 canvas.create_image(width/2, height/2, image=image)
 
 right_frame = tk.Frame(frame, bg="white")
 test = tk.Label(right_frame, text="coucou")
-test.pack()
+test.pack(expand=tk.YES, fill=tk.BOTH, side=tk.RIGHT)
 
-canvas.grid(row=1, column=0, columnspan=1)
-right_frame.grid(row=1, column=1, columnspan=1)
-header.grid(row=0, column=0,columnspan=2, sticky=tk.EW, ipady=20, ipadx=20)
+canvas.grid(row=0, column=0)
+right_frame.grid(row=0, column=1, sticky=tk.W)
+
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+frame.grid_columnconfigure(1, weight=1)
+
 
 
 frame.pack(expand=tk.YES, fill=tk.BOTH)
